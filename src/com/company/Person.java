24e7d37e818620ac49c9b9b728f3a50d9e688d68
aimless-cpu <s.grabher@protonmail.com>
2021-04-26 17:39:38 +0200

    added class Person

---
+++ src/com/company/Person.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Person {
+    //instance
+    private String firstName;
+    private String surName;
+    private int age;
+
+    //constructor
+    public void Person(String firstName, String surName, int age) {
+        this.firstName = firstName;
+        this.surName = surName;
+        this.age = age;
+    }
+
+    //method
+
+    //getter setter
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
